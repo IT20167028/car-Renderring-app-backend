cc09e2a21ca123fb38940e7fdbf157b7818b7adf
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2023-05-14 15:52:17 +0530

    initial commit

---
+++ server/models/vehicles/booking.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    car: { type: mongoose.Schema.Types.ObjectID, ref: "cars" },
+    user: { type: mongoose.Schema.Types.ObjectID, ref: "users" },
+    bookedTimeSlots: { from: { type: String }, to: { type: String } },
+    totalHours: { type: Number },
+    totalAmount: { type: Number },
+    transactionId: { type: String },
+    driverRequired: { type: Boolean },
+    bookingStatus: { type: String, default: "Pending" },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const bookingModel = mongoose.model("bookings", bookingSchema);
+module.exports = bookingModel;
---
+++ server/routes/vehicles/booking.routes.js
@@ -0,0 +1,156 @@
+const express = require("express");
+const router = express.Router();
+const Booking = require("../../models/vehicles/booking.model");
+const Car = require("../../models/vehicles/car.model");
+
+router.post("/bookcar", async (req, res) => {
+  req.body.transactionId = "1234";
+  try {
+    const newbooking = new Booking(req.body);
+    await newbooking.save();
+    const car = await Car.findOne({ _id: req.body.car });
+    car.bookedTimeSlots.push(req.body.bookedTimeSlots);
+
+    await car.save();
+    res.status(200).json({
+      status: true,
+      message: "Car booked successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
+
+router.get("/getallbookings", async (req, res) => {
+  try {
+    const bookings = await Booking.find()
+      .populate(["car", "user"])
+      .sort({ createdAt: -1 });
+    res.status(200).json({
+      status: true,
+      bookings,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
+
+router.get("/getallbookings/:id", async (req, res) => {
+  try {
+    const userID = req.params.id;
+    const bookings = await Booking.find({ user: userID })
+      .populate("car")
+      .sort({ createdAt: -1 });
+
+    if (bookings.length == 0) {
+      return res.status(400).json({
+        status: false,
+        message: "No bookings found",
+      });
+    } else {
+      res.status(200).json({
+        status: true,
+        bookings,
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
+
+router.delete("/deletebooking/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const booking = await Booking.findById(id);
+
+    if (!booking) {
+      return res.status(400).json({
+        status: false,
+        message: "Booking not found",
+      });
+    } else {
+      const car = await Car.findById(booking.car);
+      car.bookedTimeSlots.pull(booking.bookedTimeSlots);
+      await car.save();
+      await Booking.findByIdAndDelete(id);
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "Booking deleted successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
+
+router.get("/getcarbookings/:id", async (req, res) => {
+  try {
+    const carID = req.params.id;
+    const bookings = await Booking.find({ car: carID });
+    var temp = [];
+    for (var booking of bookings) {
+      temp.push(booking.bookedTimeSlots);
+    }
+    if (bookings.length == 0) {
+      return res.status(400).json({
+        status: false,
+        bookings: temp,
+        message: "No bookings found",
+      });
+    } else {
+      res.status(200).json({
+        status: true,
+        bookings: temp,
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
+
+//change booking status
+router.patch("/confirmBooking/:id", async (req, res) => {
+  try {
+    const bookongID = req.params.id;
+
+    const booking = await Booking.findById(bookongID);
+
+    if (booking) {
+      booking.bookingStatus = "Confirmed";
+      await booking.save();
+      return res.status(200).json({
+        status: true,
+        message: "Booking confirmed",
+      });
+    } else {
+      return res.status(404).json({
+        status: false,
+        message: "Booking not found",
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
